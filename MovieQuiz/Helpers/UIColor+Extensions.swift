653b9fd6a914fcfdacf07cbbe86219bb9dd70d1c
Eduard <eduardkarimov.rb@gmail.com>
2023-05-28 03:16:05 +0500

    new futures in the project

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,3 +1,18 @@
 import UIKit
 
-extension UIColor { }
+extension UIColor {
+       static var ypBlack: UIColor { UIColor(named: "dark") ?? UIColor.black }
+    static var ypGreen: UIColor {
+        UIColor(named: "green") ?? UIColor.green
+    }
+    static var ypRed: UIColor {
+        UIColor(named: "red") ?? UIColor.red
+    }
+    static var ypGrey: UIColor {
+        UIColor(named: "grey") ?? UIColor.lightGray
+    }
+    static var ypWhite: UIColor {UIColor.white}
+    static var ypBackground: UIColor {
+        UIColor(named: "background") ?? UIColor.darkGray
+    }
+}

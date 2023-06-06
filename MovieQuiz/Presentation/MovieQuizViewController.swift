e839322fb17109815038962a014b023d425f0792
Eduard <eduardkarimov.rb@gmail.com>
2023-06-06 12:23:52 +0500

    fix bugs

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -9,101 +9,10 @@ final class MovieQuizViewController: UIViewController {
     @IBOutlet weak private var image: UIImageView!
     @IBOutlet weak private var questionLable: UILabel!
     @IBOutlet weak private var counterL: UILabel!
-    
-    // функция, которая выводит вопрос
-    private func show(quiz step: QuizStepViewModel) {
-        image.image = step.image
-        questionLable.text = step.question
-        counterL.text = step.questionNumber
-    }
-    
-    struct QuizResultsViewModel {
-      let title: String
-      let text: String
-      let buttonText: String
-    }
-    
-    // функция для вывода аллерта
-    private func show(quiz result: QuizResultsViewModel) {
-        let alert = UIAlertController(
-            title: "Этот раунд окончен!",
-            message: "Ваш результат:\(correctAnswers)/10",
-            preferredStyle: .alert)
 
-        let action = UIAlertAction(title: result.buttonText, style: .default) { _ in
-            currentQuestionIndex = 0
-            // сбрасываем переменную с количеством правильных ответов
-            correctAnswers = 0
-            
-            // заново показываем первый вопрос
-            let firstQuestion = questions[currentQuestionIndex]
-            let viewModel = convert(model: firstQuestion)
-            self.show(quiz: viewModel)
-        }
-
-        alert.addAction(action)
-
-        self.present(alert, animated: true, completion: nil)
-    }
-    
-    //функция, которая показывает следующий вопрос или результат
-    private func showNextQuestionOrResults() {
-        if currentQuestionIndex == questions.count - 1 {
-            let text = "Ваш результат: \(correctAnswers)/10"
-            let viewModel = QuizResultsViewModel(
-                title: "Этот раунд окончен!",
-                text: text,
-                buttonText: "Сыграть ещё раз")
-            show(quiz: viewModel)
-        } else {
-            currentQuestionIndex += 1
-            let nextQuestion = questions[currentQuestionIndex]
-            let viewModel = convert(model: nextQuestion)
-            
-            show(quiz: viewModel)
-        }
-    }
-    
-    //функция, которая выводит результат ответа
-    private func showAnswerResult(isCorrect: Bool) {
-        if isCorrect {
-                correctAnswers += 1
-            }
-        
-        image.layer.masksToBounds = true
-        image.layer.borderWidth = 8
-        image.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
-        
-        // запускаем задачу через 1 секунду c помощью диспетчера задач
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-           // код, который мы хотим вызвать через 1 секунду
-            self.showNextQuestionOrResults()
-        }
-    }
-    
-    
-    // Кнопка да
-    @IBAction private func buttonYes(_ sender: Any) {
-        let currentQuestion = questions[currentQuestionIndex]
-            let givenAnswer = true
-            
-            showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
-    }
-    
-    // Кнопка нет
-    @IBAction private func buttonNo(_ sender: Any) {
-        let currentQuestion = questions[currentQuestionIndex]
-            let givenAnswer = false
-            
-            showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
-        }
-    }
-    
-    struct QuizQuestion {
-        let image: String
-        let text: String
-        let correctAnswer: Bool
-    }
+    // приватные переменные
+    private var currentQuestionIndex = 0
+    private var correctAnswers = 0
     
     // мок данные
     private let questions: [QuizQuestion] = [
@@ -139,16 +48,25 @@ final class MovieQuizViewController: UIViewController {
                       correctAnswer: false)
     ]
     
-// обнуляем индекс правильного ответа и счетчик корректных ответов
-    private var currentQuestionIndex = 0
-    private var correctAnswers = 0
-
-    struct QuizStepViewModel {
-      let image: UIImage
-      let question: String
-      let questionNumber: String
+    struct QuizQuestion {
+        let image: String
+        let text: String
+        let correctAnswer: Bool
     }
-
+    
+    struct QuizStepViewModel {
+        let image: UIImage
+        let question: String
+        let questionNumber: String
+    }
+    
+    struct QuizResultsViewModel {
+        let title: String
+        let text: String
+        let buttonText: String
+    }
+    
+    //функция, которая конвертирует мок данные
     private func convert(model: QuizQuestion) -> QuizStepViewModel {
         let questionStep = QuizStepViewModel(
             image: UIImage(named: model.image) ?? UIImage(),
@@ -156,13 +74,84 @@ final class MovieQuizViewController: UIViewController {
             questionNumber: "\(currentQuestionIndex + 1)/\(questions.count)")
         return questionStep
     }
-
-    // функция с переходом в один из сценариев
-    private func showNextQuestionOrResults() {
-        if currentQuestionIndex == questions.count - 1 { // 1
-            // идём в состояние "Результат квиза"
-        } else { // 2
-            currentQuestionIndex += 1
-            // идём в состояние "Вопрос показан"
+    
+    // функция, которая выводит первый вопрос
+    private func show(quiz step: QuizStepViewModel) {
+        image.image = step.image
+        questionLable.text = step.question
+        counterL.text = step.questionNumber
+    }
+    
+    //функция, которая выводит результат ответа
+    private func showAnswerResult(isCorrect: Bool) {
+        if isCorrect {
+            correctAnswers += 1
+        }
+        
+        image.layer.masksToBounds = true
+        image.layer.borderWidth = 8
+        image.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
+        
+        // запускаем задачу через 1 секунду c помощью диспетчера задач
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            // код, который мы хотим вызвать через 1 секунду
+            self.showNextQuestionOrResults()
         }
     }
+    
+    // функция для вывода аллерта
+    private func show(quiz result: QuizResultsViewModel) {
+        let alert = UIAlertController(
+            title: "Этот раунд окончен!",
+            message: "Ваш результат:\(correctAnswers)/10",
+            preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: result.buttonText, style: .default) { _ in
+            // заново показываем первый вопрос
+            let firstQuestion = self.questions[self.currentQuestionIndex]
+            let viewModel = self.convert(model: firstQuestion)
+            self.show(quiz: viewModel)
+        }
+        
+        alert.addAction(action)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    //функция, которая перезапускает квиз
+    private func showNextQuestionOrResults() {
+        if currentQuestionIndex == questions.count - 1 {
+            let text = "Ваш результат: \(correctAnswers)/10"
+            let viewModel = QuizResultsViewModel(
+                title: "Этот раунд окончен!",
+                text: text,
+                buttonText: "Сыграть ещё раз")
+            show(quiz: viewModel)
+        } else {
+            currentQuestionIndex += 1
+            let nextQuestion = questions[currentQuestionIndex]
+            let viewModel = convert(model: nextQuestion)
+            
+            show(quiz: viewModel)
+        }
+    }
+    
+    
+    // Кнопка да
+    @IBAction private func buttonYes(_ sender: Any) {
+        let currentQuestion = questions[currentQuestionIndex]
+        let givenAnswer = true
+        
+        
+        showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
+        
+    }
+    
+    // Кнопка нет
+    @IBAction private func buttonNo(_ sender: Any) {
+        let currentQuestion = questions[currentQuestionIndex]
+        let givenAnswer = false
+        
+        showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
+    }
+}

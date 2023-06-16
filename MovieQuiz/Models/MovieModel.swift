40c148cc5a45d50632cbecf0bba6f419bfda15e9
Eduard <eduardkarimov.rb@gmail.com>
2023-06-16 23:43:38 +0500

    add new futures

--- MovieQuiz/Models/MovieModel.swift
+++ MovieQuiz/Models/MovieModel.swift
@@ -16,14 +16,50 @@ struct Movie {
     let runtimeMins: Int
     let directors: String
     let actorList: [Actor]
+    
+    struct Actor {
+        let id: String
+        let image: String
+        let name: String
+        let asCharacter: String
+    }
+    
+    enum CodingKeys: CodingKey {
+       case id, title, year, image, releaseDate, runtimeMins, directors
+       case actorList
+     }
+    
+    enum ParseError: Error {
+        case yearFailure
+        case runtimeMinsFailure
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        id = try container.decode(String.self, forKey: .id)
+        title = try container.decode(String.self, forKey: .title)
+        
+        let year = try container.decode(String.self, forKey: .year)
+        guard let yearValue = Int(year) else {
+            throw ParseError.yearFailure
+        }
+        self.year = yearValue
+        
+        image = try container.decode(String.self, forKey: .image)
+        releaseDate = try container.decode(String.self, forKey: .releaseDate)
+        
+        let runtimeMins = try container.decode(String.self, forKey: .runtimeMins)
+        guard let runtimeMinsValue = Int(runtimeMins) else {
+            throw ParseError.runtimeMinsFailure
+        }
+        self.runtimeMins = runtimeMinsValue
+        
+        directors = try container.decode(String.self, forKey: .directors)
+        actorList = try container.decode([Actor].self, forKey: .actorList)
+    }
 }
 
-struct Actor {
-    let id: String
-    let image: String
-    let name: String
-    let asCharacter: String
-}
 
 func getMovie(from jsonString: String) -> Movie? {
     var movie: Movie? = nil
